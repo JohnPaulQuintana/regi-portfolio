dc43fc46ce326c0544bbff5c4c76c7a3cc53581e
John Paul Quintana <jpquintana2024@gmail.com>
2024-12-27 14:57:39 +0800

    Update Marquee.jsx

--- src/app/components/Marquee.jsx
+++ src/app/components/Marquee.jsx
@@ -9,73 +9,73 @@ const MarqueeSection = () => {
                 <div className="item">
                   <h4 className="d-flex align-items-center">
                     <span className="">Senior Graphic Designer</span>
-                    <span className="text-2xl ml-50 stroke icon">/</span>
-                  </h4>
-                </div>
-                <div className="item">
-                  <h4 className="d-flex align-items-center">
-                    <span className="text-color-primary-green">Dynamic Video Producer</span>
-                    <span className="ml-50 stroke icon">01</span>
-                  </h4>
-                </div>
-                <div className="item">
-                  <h4 className="d-flex align-items-center">
-                    <span>Full-Stack Web Developer</span>
-                    <span className="text-2xl ml-50 stroke icon">02</span>
-                  </h4>
-                </div>
-                <div className="item">
-                  <h4 className="d-flex align-items-center">
-                    <span className="">Senior Graphic Designer</span>
-                    <span className="text-2xl ml-50 stroke icon">03</span>
-                  </h4>
-                </div>
-                <div className="item">
-                  <h4 className="d-flex align-items-center">
-                    <span className="text-color-primary-green">Dynamic Video Producer</span>
                     <span className="text-2xl ml-50 stroke icon">01</span>
                   </h4>
                 </div>
                 <div className="item">
                   <h4 className="d-flex align-items-center">
-                    <span>Full-Stack Web Developer</span>
-                    <span className="text-2xl ml-50 stroke icon">02</span>
+                    <span className="text-color-primary-green">Dynamic Video Producer</span>
+                    <span className="ml-50 stroke icon">02</span>
                   </h4>
                 </div>
                 <div className="item">
                   <h4 className="d-flex align-items-center">
-                    <span className="">Senior Graphic Designer</span>
+                    <span>Full-Stack Web Developer</span>
                     <span className="text-2xl ml-50 stroke icon">03</span>
                   </h4>
                 </div>
                 <div className="item">
                   <h4 className="d-flex align-items-center">
-                    <span className="text-color-primary-green">Dynamic Video Producer</span>
+                    <span className="">Senior Graphic Designer</span>
                     <span className="text-2xl ml-50 stroke icon">01</span>
                   </h4>
                 </div>
                 <div className="item">
                   <h4 className="d-flex align-items-center">
-                    <span>Full-Stack Web Developer</span>
+                    <span className="text-color-primary-green">Dynamic Video Producer</span>
                     <span className="text-2xl ml-50 stroke icon">02</span>
                   </h4>
                 </div>
                 <div className="item">
                   <h4 className="d-flex align-items-center">
-                    <span className="">Senior Graphic Designer</span>
+                    <span>Full-Stack Web Developer</span>
                     <span className="text-2xl ml-50 stroke icon">03</span>
                   </h4>
                 </div>
                 <div className="item">
                   <h4 className="d-flex align-items-center">
-                    <span className="text-color-primary-green">Dynamic Video Producer</span>
+                    <span className="">Senior Graphic Designer</span>
                     <span className="text-2xl ml-50 stroke icon">01</span>
                   </h4>
                 </div>
                 <div className="item">
                   <h4 className="d-flex align-items-center">
-                    <span>Full-Stack Web Developer</span>
+                    <span className="text-color-primary-green">Dynamic Video Producer</span>
                     <span className="text-2xl ml-50 stroke icon">02</span>
+                  </h4>
+                </div>
+                <div className="item">
+                  <h4 className="d-flex align-items-center">
+                    <span>Full-Stack Web Developer</span>
+                    <span className="text-2xl ml-50 stroke icon">03</span>
+                  </h4>
+                </div>
+                <div className="item">
+                  <h4 className="d-flex align-items-center">
+                    <span className="">Senior Graphic Designer</span>
+                    <span className="text-2xl ml-50 stroke icon">01</span>
+                  </h4>
+                </div>
+                <div className="item">
+                  <h4 className="d-flex align-items-center">
+                    <span className="text-color-primary-green">Dynamic Video Producer</span>
+                    <span className="text-2xl ml-50 stroke icon">02</span>
+                  </h4>
+                </div>
+                <div className="item">
+                  <h4 className="d-flex align-items-center">
+                    <span>Full-Stack Web Developer</span>
+                    <span className="text-2xl ml-50 stroke icon">03</span>
                   </h4>
                 </div>
                 
